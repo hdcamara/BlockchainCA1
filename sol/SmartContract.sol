b07c5508b95d186e3d1e0503cc236e91fed58bb0
Helder Camara <100359684+hdcamara@users.noreply.github.com>
2023-12-01 13:57:03 +0000

    Update (strings) SmartContract.sol

--- sol/SmartContract.sol
+++ sol/SmartContract.sol
@@ -17,3 +17,18 @@ contract ClothingManufacture {
         string quantity;
         string companyAddress;
         Status status;
+
+
+Manufacturer public manufacturer;
+    mapping(string => Batch) private batches;
+    mapping(address => string[]) private producerBatches;
+
+    modifier onlyManufacturer() {
+        require(msg.sender == manufacturer.manufacturerAddress, "Only the manufacturer can call this function.");
+        _;
+    }
+
+    constructor(string memory _manufacturerName) {
+        manufacturer.manufacturerAddress = msg.sender;
+        manufacturer.manufacturerName = _manufacturerName;
+    }
